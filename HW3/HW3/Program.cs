5b66919b2a9ac29d8f2b348570d203a039d53998
JordanMC8 <jordancuison099@gmail.com>
2025-10-01 23:00:21 +0800

    adding HW3

---
+++ HW3/HW3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    class Program
+    {
+        static Dictionary<char, int> frequency(string input)
+        {
+            Dictionary<char, int> freq = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (c == ' ' || c == ',') 
+                    continue;
+
+                if (freq.ContainsKey(c))
+                    freq[c]++;
+                else
+                    freq[c] = 1;
+            }
+
+            return freq;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string input = Console.ReadLine();
+
+            
+            string[] parts = input.Split(',');
+
+            foreach (string part in parts)
+            {
+                string word = part.Trim(); 
+                Dictionary<char, int> result = frequency(word);
+
+                
+                foreach (var e in result)
+                {
+                    Console.Write($"{e.Key}={e.Value}, ");
+                }
+                Console.WriteLine(); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
